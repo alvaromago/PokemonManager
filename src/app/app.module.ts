62a7436bd2f1c3edef49efe80c5dfd7186c8cfeb
alvaromago <aalvaromago@gmail.com>
2024-01-10 10:38:08 +0100

    finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { GeneracionesComponent } from './components/generaciones/generaciones.component';
+import { PokemonsComponent } from './components/pokemons/pokemons.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GeneracionesComponent,
+    PokemonsComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/generaciones/generaciones.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-generaciones',
+  templateUrl: './generaciones.component.html',
+  styleUrls: ['./generaciones.component.css']
+})
+export class GeneracionesComponent {
+
+}
