206b1078253e539eba11ad63285936b8bee87cee
alvaromago <aalvaromago@gmail.com>
2024-01-17 11:08:28 +0100

    comienzo practica 2

--- src/app/components/generaciones/generaciones.component.ts
+++ src/app/components/generaciones/generaciones.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-generaciones',
   templateUrl: './generaciones.component.html',
   styleUrls: ['./generaciones.component.css']
 })
-export class GeneracionesComponent {
-
+export class GeneracionesComponent implements OnInit{
+  generaciones = ["Generación I", "Generación II", "Generación III", "Generación IV", "Generación V", "Generación VI", "Generación VII"];
+  
+  constructor(){}
+  ngOnInit(): void {}
 }
