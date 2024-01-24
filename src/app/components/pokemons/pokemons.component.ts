d3df33987035003a2d037a20bc208288c39ece4d
alvaromago <aalvaromago@gmail.com>
2024-01-24 18:17:01 +0100

    practica t7

--- src/app/components/pokemons/pokemons.component.ts
+++ src/app/components/pokemons/pokemons.component.ts
@@ -9,13 +9,13 @@ export class PokemonsComponent implements OnInit {
     ngOnInit(): void { }
     constructor() { }
 
-    Pokemons = [
-        { id: 1, nombre: 'Bulbasaur', categoria: 'Grano', tipo: 'Planta/Veneno', talla: 70, peso: 6900 },
-        { id: 2, nombre: 'Ivysaur', categoria: 'Grano', tipo: 'Planta/Veneno', talla: 100, peso: 13000 },
-        { id: 3, nombre: 'Venusaur', categoria: 'Grano', tipo: 'Planta/Veneno', talla: 200, peso: 100000 },
-        { id: 4, nombre: 'Charmander', categoria: 'Lagarto', tipo: 'Fuego', talla: 60, peso: 8500 },
-        { id: 5, nombre: 'Charmeleon', categoria: 'Llama', tipo: 'Fuego', talla: 110, peso: 19000 },
-        { id: 6, nombre: 'Charizard', categoria: 'Llama', tipo: 'Fuego/Volador', talla: 170, peso: 90500 }
+    pokemons = [
+        { id: 1, nombre: 'Bulbasaur', categoria: 'Grano', tipo: 'Planta/Veneno', talla: 0, peso: 6900, img: './assets/imgPokemons/1.png' },
+        { id: 2, nombre: 'Ivysaur', categoria: 'Grano', tipo: 'Planta/Veneno', talla: 100, peso: 13000, img: "./assets/imgPokemons/2.png" },
+        { id: 3, nombre: 'Venusaur', categoria: 'Grano', tipo: 'Planta/Veneno', talla: 200, peso: 100000, img: "./assets/imgPokemons/3.png" },
+        { id: 4, nombre: 'Charmander', categoria: 'Lagarto', tipo: 'Fuego', talla: 60, peso: 8500, img: "./assets/imgPokemons/4.png" },
+        { id: 5, nombre: 'Charmeleon', categoria: 'Llama', tipo: 'Fuego', talla: 110, peso: 19000, img: "./assets/imgPokemons/5.png" },
+        { id: 6, nombre: 'Charizard', categoria: 'Llama', tipo: 'Fuego/Volador', talla: 170, peso: 90500, img: "./assets/imgPokemons/6.png" }
     ];
 
     formatearNumero(num: number): string {
